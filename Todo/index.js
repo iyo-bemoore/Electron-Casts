714244b86272b937f5b576b633e69ece3058c58e
2imad <imadyoussoufi@gmail.com>
2019-06-15 08:35:56 +0200

    add TODO

--- Todo/index.js
+++ Todo/index.js
@@ -3,8 +3,46 @@ const electron = require('electron');
 const { app, BrowserWindow , Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', ( ) => {
+
   mainWindow =  new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
-})
+  mainWindow.on('closed', () => app.quit());
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu); 
+
+});
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width : 300,
+    height: 200,
+    title : "Add New Todo"
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`)
+}
+
+
+const menuTemplate = [
+  {
+    label : "File",
+    submenu: [
+      {
+        label: "New Todo",
+        click() {
+          createAddWindow()
+        } 
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform === 'darwin' ? 'Command+Q':"Ctrl+Q",
+        click() {
+          app.quit()
+        }
+      }
+    ]
+  }
+];
