8994597e0f0853fa0ec49e6fb701f8d1a3b6178b
2imad <imadyoussoufi@gmail.com>
2019-06-15 11:41:32 +0200

    update webpack

---
+++ tasky/index.js
@@ -0,0 +1,14 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+
+let mainWindow;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+        nodeIntegration: true
+  }
+
+    });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+})
--- tasky/src/components/App.js
+++ tasky/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-
 import Header from "./Header";
 import TasksIndex from "./TasksIndex";
 import TasksShow from "./TasksShow";
