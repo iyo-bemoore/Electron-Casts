d64addab4e1cdeb6d995f90c29d0d44d60563dd9
2imad <imadyoussoufi@gmail.com>
2019-06-15 17:36:09 +0200

    refactor Tray class

---
+++ tasky/app/TimerTray.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const { Tray } = electron;
+
+class TimerTray extends Tray {
+    constructor(iconPath,mainWindow){
+        super(iconPath);
+        this.mainWindow = mainWindow 
+        this.on('click', this.onClick.bind(this));
+    }
+
+    onClick(event , bounds) {
+        const { x, y } = bounds;
+        //window height and width
+        const { height, width } = this.mainWindow.getBounds();
+        if (this.mainWindow.isVisible()) {
+            this.mainWindow.hide();
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            this.mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height: height,
+                width: width
+            })
+            this.mainWindow.show();
+        }
+    }
+
+}
+
+
+module.exports = TimerTray;
--- tasky/index.js
+++ tasky/index.js
@@ -1,14 +1,28 @@
+const path = require('path');
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const { app, BrowserWindow, Tray } = electron;
+const TimerTray = require('./app/TimerTray');
 
 
 let mainWindow;
+let tray;
+
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         webPreferences: {
-        nodeIntegration: true
-  }
-
+            nodeIntegration: true
+        },
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false,
+        show: false,
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+
+    const iconName = process.platform === 'win32' || 'linux' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
+    tray = new TimerTray(iconPath,mainWindow);
+        
 })
